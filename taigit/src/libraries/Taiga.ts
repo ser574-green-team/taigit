b9f963a40b04b5b28af180d67b8c3425f25f9149
John Alden <jzalden96@gmail.com>
2019-02-16 17:54:25 -0700

    Introduce Base Taiga Architecture

---
+++ taigit/src/libraries/Taiga.ts
@@ -0,0 +1,12 @@
+import axios from "axios"
+
+export async function
+taiga_login(username : string, password : string) : Promise<boolean> {
+    let response = await axios.post("https://api.taiga.io/api/v1/auth", {
+        "password": password,
+        "type": "normal",
+        "username": username
+    })
+
+    return (response.status == 200);
+}
