a5e63dcbab7d2b852d0b5b4e4910b62d8de7c702
xiangwei <xzheng80@asu.edu>
2019-02-17 14:49:50 -0700

    add function to return sprint information for a given project for US18Task72

--- taigit/src/components/Taiga.js
+++ taigit/src/components/Taiga.js
@@ -1,11 +1,85 @@
 import React, { Component } from 'react';
 
+import {taiga_login, taiga_sprint} from '../libraries/Taiga'
+
 export default class Taiga extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      password: '',
+      //slug:'',//type of Taiga
+    };
+
+    this.on_change = this.on_change.bind(this);
+    this.on_submit = this.on_submit.bind(this);
+  }
+
+  on_change(event) {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  }
+
+  on_submit(event) {
+    taiga_login(this.state.username, this.state.password).then((ret)=>{
+      if(ret) alert("taiga login: successful");
+      else    alert("taiga login: unsuccessful");
+    });
+    event.preventDefault();
+  }
+    /*
+    on_us(event) {
+        taiga_sprint(this.state.username).then((ret)=>{
+            if(ret) alert("taiga us: successful");
+            else    alert("taiga us: unsuccessful");
+        });
+        event.preventDefault();
+    }
+   */
+
   render() {
     return(
-      <div className="app-page">
-        <h2>Taiga</h2>
-      </div>
+        <div className="app-page">
+          <h2>Taiga</h2>
+
+          <form onSubmit={this.on_submit}>
+            <label>
+              Username:
+              <input
+                  type="text"
+                  name="username"
+                  value={this.state.username}
+                  onChange={this.on_change} />
+            </label><br/>
+
+            <label>
+              Password:
+              <input
+                  type="password"
+                  name="password"
+                  value={this.state.password}
+                  onChange={this.on_change} />
+            </label><br/>
+
+            <input type="submit" value="Submit" />
+          </form>
+            /*
+            <form onUs={this.on_us}>
+                <label>
+                    Slug:
+                    <input
+                        type="text"
+                        name="slug"
+                        value={this.state.slug}
+                        onChange={this.on_change} />
+                </label><br/>
+                <input type="submit" value="Submit" />
+                */
+            </form>
+        </div>
+
+
     );
   }
 }
--- taigit/src/libraries/Taiga.ts
+++ taigit/src/libraries/Taiga.ts
@@ -10,3 +10,12 @@ taiga_login(username : string, password : string) : Promise<boolean> {
 
     return (response.status == 200);
 }
+
+
+export async function
+taiga_sprint(slug: string) :  Promise<Object>{
+    let url2 = "https://api.taiga.io/api/v1";
+    let response = await axios.get(url2+"/projects/by_slug?slug="+slug) ;
+
+    return (response);
+}
