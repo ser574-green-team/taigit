d5c123ff234d24956fde863045d2e8fdd6a3b1e4
Trevor Forrey <trevor4e@gmail.com>
2019-02-17 22:20:19 -0700

    US-10 Task50&51 Converted Overview charts to different types

--- taigit/src/components/Overview.js
+++ taigit/src/components/Overview.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import LineChart from './charts/LineChart';
+import NumberDisplay from './NumberDisplay'
 import DoughnutChart from './charts/DoughnutChart';
 
 export default class Overview extends Component {
@@ -7,45 +8,24 @@ export default class Overview extends Component {
     return(
       <div className="app-page">
         <h2>Overview</h2>
-          <div className="chart chart-pie">
-            <span className="chart-title">Number of Commits</span>
-            <DoughnutChart chartData={numberOfCommits} options={{}}/>
-          </div>
+        <NumberDisplay number="43" statistic="Total Commits"/>
         <div className="chart chart-vertical-primary">
-          <span className="chart-title">Technologies Used </span>
-          <LineChart chartData={technologiesUsed} options={{}}/>
+          <DoughnutChart title="Technologies Used" chartData={technologiesUsed} options={{}}/>
         </div>
       </div>
     );
   }
 }
 
-
-
 let technologiesUsed = {
-  labels: ["Front-End", "Back-End", "Database"],
+  labels: ["ReactJS", "Python", "HTML"],
   datasets: [{
     label: 'Technologies Used',
-    data: ['ReactJS', 'Python', 'SQL'],
+    data: [6, 4, 3],
     backgroundColor: [
         'rgb(43, 175, 204, 1)',
         'rgb(62, 273, 99, 1)',
         'rgb(13, 206, 110, 1)',
-    ],
-  }]
-}
-
-let numberOfCommits = {
-  labels: ["01/31", "02/05", "02/10", "02/15", "02/17"],
-  datasets: [{
-    label: 'Number of Commits',
-    data: [5, 14, 21, 46, 130],
-    backgroundColor: [
-        'rgb(73, 63, 74, 1)',
-        'rgb(163, 45, 124, 1)',
-        'rgb(143, 75, 104, 1)',
-        'rgb(242, 73, 69, 1)',
-        'rgb(23, 26, 210, 1)',
     ],
   }]
 }
