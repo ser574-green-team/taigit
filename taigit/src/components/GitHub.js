08ff869f429a378381ffa1ac2ed1aeb823433dba
Ruby Zhao <rubyqzhao@gmail.com>
2019-02-17 21:04:47 -0700

    Added components for pull requests and commits per member to the GitHub page (US# 54, 56, 57)

--- taigit/src/components/GitHub.js
+++ taigit/src/components/GitHub.js
@@ -1,23 +1,24 @@
 import React, { Component } from 'react'
 import BarChart from './charts/BarChart';
+import NumberDisplay from './NumberDisplay'
+import barChartData from './charts/barChartData'
 
 export default class GitHub extends Component {
   render() {
     return(
       <div className="app-page">
         <h2>GitHub</h2>
-        <div className="chart chart-horizontal-primary">
-        <BarChart/>       
-        </div>
-        <div className="chart chart-square-primary"></div>
-        <div className="chart chart-horizontal-primary"></div>
-        <div className="chart chart-square-primary"></div>
-        <div className="chart chart-horizontal-primary"></div>
-        <div className="chart chart-square-primary"></div>
-        <div className="chart chart-square-primary"></div>
-        <div className="chart chart-horizontal-primary"></div>
-        <div className="chart chart-square-primary">
-        </div>
+
+          <div className="chart chart-bar">
+              <span className="chart-title">Commits Per Member</span>
+              <BarChart chartData={barChartData}/>
+          </div>
+
+          <div className="pull-req-stats">
+              <NumberDisplay number="13" statistic="Pull Requests Created"/>
+              <NumberDisplay number="6" statistic="Pull Requests Reviewed"/>
+          </div>
+
       </div>
     );
   }
