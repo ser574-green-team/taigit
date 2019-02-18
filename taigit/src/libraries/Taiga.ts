187154cba0f6e74336714f69644add266247be34
Cecilia <laplace95@gmail.com>
2019-02-17 21:47:55 -0700

    Added project info call

--- taigit/src/libraries/Taiga.ts
+++ taigit/src/libraries/Taiga.ts
@@ -10,3 +10,9 @@ taiga_login(username : string, password : string) : Promise<boolean> {
 
     return (response.status == 200);
 }
+export async function
+project_info(slug : string) : Promise<boolean> {
+    let response = await axios.get("https://api.taiga.io/api/v1/projects/by_slug?slug=" + slug)
+
+    return (response.status == 200);
+}
