537aa01f8a21ba66afe8e03bbe79f77b62fa69b7
bpanda1 <bpanda1@asu.edu>
2019-02-18 00:02:33 -0700

    US20-task35: Created the API to get the count of task per user in Taiga

--- Taiga/src/GetMileStoneIds.ts
+++ Taiga/src/GetMileStoneIds.ts
@@ -6,18 +6,57 @@ export class GetMileStoneIds {
         let sprintNameArray: any;
         sprintDetailsArray = respForFutureExtraction.milestones;
         let sprintDetail:any;
+        let sprintId:any;
         for (sprintDetail of sprintDetailsArray) {
-            let sprintId = sprintDetail.id;
-            console.log(sprintId);
+            sprintId = sprintDetail.id;
+            //console.log(sprintId);
             // sprintId will be used to get all the sprint details
+
             this.printSprintIds(tokenId, sprintId, (projectId: any) => {
                 // we need to call task details here.
-                console.log("projectId :: " + projectId);
-            });;
+                //console.log(sprintId + " projectId :: " + projectId);
+                this.printTaskCount(tokenId, sprintId, projectId, (taskCount:any) => {
+                    //console.log("taskCount:any :: " + taskCount);
+                    for(let key in taskCount) {
+                        let value = taskCount[key];
+                        console.log( "For " + value + ", " + key);
+                       
+                      }
+                }); 
+            });
             
         }
     }
 
+    printTaskCount (tokenId: number, sprintId: string, projectId : any, callB:(taskCount:any) => any) {
+        let tok = 'Bearer ' + tokenId;
+        let options: any = {
+            headers: { 
+                'Content-Type': 'application/json',
+                'Authorization' : tok,
+            },
+            json: true
+        }
+        let dict:any = {};
+        //console.log("task   " + sprintId + "  " + projectId);
+        request.get('https://api.taiga.io/api/v1/tasks?milestone=' + sprintId + "&&project=" + projectId , options, (error:any, response: any, body:any) => {
+            for (let taskName of body) {
+                let name = taskName.assigned_to_extra_info.full_name_display;
+                let sub = taskName.subject;
+                //console.log( "name :: " +name);
+                let x:any;
+                if( name in dict) {
+                    x = dict[name];
+                    dict[name] = x+1
+                } else {
+                    dict[name] = 1;
+                }
+
+            }
+            callB(dict);
+            
+        });
+    }
     printSprintIds(tokenId: number, sprintId: string, callB:(projectId:any) => any) {
         let tok = 'Bearer ' + tokenId;
         let options: any = {
@@ -29,10 +68,9 @@ export class GetMileStoneIds {
         }
 
         request.get('https://api.taiga.io/api/v1/milestones/' + sprintId, options, (error:any, response: any, body:any) => {
-            console.log("body.project_extra_info.id: " + body.project_extra_info.id);
             callB(body.project_extra_info.id);
             
         });
-    }
+    } 
     
 }
