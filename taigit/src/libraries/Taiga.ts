3e27e2a6007e77cb46977345e2097f14fac23991
Cecilia <laplace95@gmail.com>
2019-02-18 00:09:52 -0700

    Isolated project information

--- taigit/src/libraries/Taiga.ts
+++ taigit/src/libraries/Taiga.ts
@@ -10,9 +10,12 @@ taiga_login(username : string, password : string) : Promise<boolean> {
 
     return (response.status == 200);
 }
-export async function
-project_info(slug : string) : Promise<boolean> {
-    let response = await axios.get("https://api.taiga.io/api/v1/projects/by_slug?slug=" + slug)
 
-    return (response.status == 200);
+// This call returns key project information
+export async function
+project_info(slug : string) : Promise<Object> {
+    let data = await axios.get("https://api.taiga.io/api/v1/projects/by_slug?slug=" + slug)
+    let info : {id: number, name: string, slug: string, created_date: Date} = 
+        {id: data.data.id, name: data.data.name, slug: data.data.slug, created_date: data.data.created_date};
+    return (info);
 }
