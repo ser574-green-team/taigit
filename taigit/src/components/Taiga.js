2e2cd429be260c885a3e3218b2787c43cbc4bbb0
xiangwei <xzheng80@asu.edu>
2019-02-17 21:58:04 -0700

    update components/Taiga.js to resolve conflict for US18Task72

--- taigit/src/components/Taiga.js
+++ taigit/src/components/Taiga.js
@@ -1,87 +1,35 @@
 import React, { Component } from 'react';
+import StackedBarChart from './charts/StackedBarChart';
+import stackBarChartData from './charts/stackedBarChartData';
+import DoughnutChart from './charts/DoughnutChart';
 
-import {taiga_login, taiga_sprint} from '../libraries/Taiga'
+let taigaUsProgress = {
+    labels: ["Completed", "In Progress", "Not Done"],
+    datasets: [{
+        label: 'User Story Progress',
+        data: [5, 4, 13],
+        backgroundColor: [
+            'rgb(242, 105, 104, 1)',
+            'rgb(242, 173, 159, 1)',
+            'rgb(223, 226, 210, 1)',
+        ],
+    }]
+}
 
 export default class Taiga extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      username: '',
-      password: '',
-      //slug:'',//type of Taiga
-    };
-
-    this.on_change = this.on_change.bind(this);
-    this.on_submit = this.on_submit.bind(this);
-  }
-
-  on_change(event) {
-    this.setState({
-      [event.target.name]: event.target.value
-    });
-  }
-
-  on_submit(event) {
-    taiga_login(this.state.username, this.state.password).then((ret)=>{
-      if(ret) alert("taiga login: successful");
-      else    alert("taiga login: unsuccessful");
-    });
-    event.preventDefault();
-  }
-
-    /*
-    //testing taiga_sprint function
-    on_us(event) {
-        taiga_sprint(this.state.username).then((ret)=>{
-            if(ret) alert("taiga us: successful");
-            else    alert("taiga us: unsuccessful");
-        });
-        event.preventDefault();
+    render() {
+        return(
+            <div className="app-page">
+                <h2>Taiga</h2>
+                <div className="chart chart-pie">
+                    <span className="chart-title">User Stories</span>
+                    <DoughnutChart chartData={taigaUsProgress} options={{}}/>
+                </div>
+                <div className="chart chart-stacked-bar">
+                    <span className="chart-title">Taiga Tasks</span>
+                    <StackedBarChart chartData={stackBarChartData}/>
+                </div>
+            </div>
+        );
     }
-   */
-
-  render() {
-    return(
-        <div className="app-page">
-          <h2>Taiga</h2>
-
-          <form onSubmit={this.on_submit}>
-            <label>
-              Username:
-              <input
-                  type="text"
-                  name="username"
-                  value={this.state.username}
-                  onChange={this.on_change} />
-            </label><br/>
-
-            <label>
-              Password:
-              <input
-                  type="password"
-                  name="password"
-                  value={this.state.password}
-                  onChange={this.on_change} />
-            </label><br/>
-
-            <input type="submit" value="Submit" />
-          </form>
-            /*
-            <form onUs={this.on_us}>
-                <label>
-                    Slug:
-                    <input
-                        type="text"
-                        name="slug"
-                        value={this.state.slug}
-                        onChange={this.on_change} />
-                </label><br/>
-                <input type="submit" value="Submit" />
-                */
-            </form>
-        </div>
-
-
-    );
-  }
 }
