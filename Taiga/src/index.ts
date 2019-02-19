c6daf9eaf061998685c99ad5785ac1f36d90a5df
bpanda1 <bpanda1@asu.edu>
2019-02-18 21:03:39 -0700

    US20-task35: Removed unnecessary calls from index file

--- Taiga/src/index.ts
+++ Taiga/src/index.ts
@@ -1,23 +1,19 @@
 import {User} from './User';
-//import {StoreResponse} from './StoreResponse';
 import {GetUserAuthToken} from './GetUserAuthToken';
 import {GetUserList} from './GetUserList';
-import {GetTaskCount} from './GetTaskCount';
 import {GetMileStoneIds} from './GetMileStoneIds';
 
 let tokenId:any ;
 let authObj = new GetUserAuthToken();
 
+//sanaydevi-ser-574
+//sarthak-scrumdevils-ser_515
+
 authObj.getUserAuthToken((user: User) => {
       tokenId = user.tokenId;
       let userListObj = new GetUserList();
-      userListObj.getUserList(tokenId, 'sarthak-scrumdevils-ser_515', (respForFutureExtraction: any) => {
-        console.log("respForFutureExtraction :: " + respForFutureExtraction.members);
-
+      userListObj.getUserList(tokenId, 'sanaydevi-ser-574', (respForFutureExtraction: any) => {
         let sprintIds = new GetMileStoneIds();
-        sprintIds.getMileStoneIds(tokenId, 'sarthak-scrumdevils-ser_515', respForFutureExtraction);
-
-
-
+        sprintIds.getMileStoneIds(tokenId, respForFutureExtraction);
       });
 });
