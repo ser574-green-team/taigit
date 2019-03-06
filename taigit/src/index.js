2dacd035ea334c53c22cafd703135f18db3a020c
Trevor Forrey <trevor4e@gmail.com>
2019-03-05 18:38:01 -0700

    [US-84 Task-90] Moved store into separate file and added middleware
    Middleware: redux-thunk and debugging enabling for Redux DevTools

--- taigit/src/index.js
+++ taigit/src/index.js
@@ -2,46 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux'; 
-import team from './reducers/team';
+import store from './store';
 
 import './index.css';
 import App from './App';
 
-let baseStore = {
-  teamMembers: [
-    {
-      name: 'Bailey Routzong',
-      taigaId: 'broutzong',
-      githubId: 'broutzong',
-      pictureUrl: 'https://baileyroutzong.com/wp-content/uploads/2015/03/circle-man.png',
-      totalCommits: 45
-    },
-    {
-      name: 'Amy Koffee',
-      taigaId: 'akoffee',
-      githubId: 'akoffee',
-      pictureUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTBhi8xXNgLziI1Orp9x_10TGtmQSnZlCOseWC_0uUh5ZMhE5QM',
-      totalCommits: 34
-    },
-    {
-      name: 'Miguel Smith',
-      taigaId: 'msmith',
-      githubId: 'msmith',
-      pictureUrl: 'https://broadstreetautoloans.com/Content/images/circle-person02.png',
-      totalCommits: 23
-    },
-    {
-      name: 'Mr Wicked',
-      taigaId: 'wickedfan',
-      githubId: 'wicked4eva',
-      pictureUrl: 'http://www.onecenter.in/wp-content/uploads/2018/07/face.png',
-      totalCommits: 34
-    }
-  ]
-};
-
-const store = createStore(team, baseStore);
 const rootElement = document.getElementById('root');
 
 ReactDOM.render(
