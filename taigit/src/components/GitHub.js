24c2a503a5ab75861dd00d69788d2416bfc8beae
Trevor Forrey <trevor4e@gmail.com>
2019-03-05 18:41:06 -0700

    [US-84 Task-90] Connecting GitHub component to redux store

--- taigit/src/components/GitHub.js
+++ taigit/src/components/GitHub.js
@@ -2,9 +2,14 @@ import React, { Component } from 'react';
 import BarChart from './charts/BarChart';
 import NumberDisplay from './NumberDisplay'
 import barChartData from './charts/barChartData'
-import * as GH from '../libraries/GitHub/GitHub';
+import { getBranchList } from '../actions/githubActions';
+import { connect } from 'react-redux';
 
-export default class GitHub extends Component {
+class GitHub extends Component {
+  componentWillMount() {
+    this.props.getBranchList();
+  }
+
   render() {
     return(
       <div className="app-page">
@@ -13,10 +18,28 @@ export default class GitHub extends Component {
               <span className="chart-title">Commits Per Member</span>
               <BarChart chartData={barChartData}/>
           </div>
-
           <NumberDisplay number="13" statistic="Pull Requests Created"/>
           <NumberDisplay number="6" statistic="Pull Requests Reviewed"/>
+          <h3>List of Branches</h3>
+          {this.props.branches.map((branchName) => {
+            return <p>{branchName}</p>
+          })}
       </div>
     );
   }
 }
+
+/**
+ * mapStateToProps
+ * maps state in redux store (right)
+ * to component props property (left)
+ */
+const mapStateToProps = state => ({
+  branches: state.github.branchesList
+});
+
+/**
+ * connect(mapStateToProps, actions)(componentName)
+ * connects the component to the redux store
+ */
+export default connect(mapStateToProps, { getBranchList })(GitHub)
