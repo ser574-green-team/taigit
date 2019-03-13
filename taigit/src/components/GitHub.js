6b1025671dbd2f49f1b07e96fd6c1c24114b8df5
Trevor Forrey <trevor4e@gmail.com>
2019-03-12 20:05:04 -0700

    [US-82 Task-91] Added Grid Layout to GitHub Page

--- taigit/src/components/GitHub.js
+++ taigit/src/components/GitHub.js
@@ -2,35 +2,49 @@ import React, { Component } from 'react';
 import BarChart from './charts/BarChart';
 import NumberDisplay from './NumberDisplay'
 import barChartData from './charts/barChartData'
+import GridLayout from 'react-grid-layout';
 import { getBranchList, getCommitsPerUser } from '../actions/githubActions';
 import { selectBranchList, selectNumCommitsChartData } from '../reducers';
 import { connect } from 'react-redux';
+import { Bar } from 'react-chartjs-2';
 
 class GitHub extends Component {
   // Calls methods in actions/githubActions to fetch data from API
   componentWillMount() {
     this.props.getBranchList();
-    this.props.getCommitsPerUser();
+    this.props.getCommitsPerUser('trevorforrey', 'OttoDB', 'trevorforrey');
   }
 
   render() {
     return(
       <div className="app-page">
         <h2>GitHub</h2>
-          <div className="chart chart-bar">
-              <span className="chart-title">Commits Per Member</span>
-              <BarChart chartData={barChartData}/>
+        <GridLayout className="layout" cols={12} rowHeight={30} width={1200}>
+          {/* <div className='box' key="1" data-grid={{ w: 2, h: 5, x: 0, y: 0, minW: 2, minH: 5 }}>
+            <div className="chart">
+                <span className="chart-title">Commits Per Member</span>
+                <Bar data={barChartData}/>
+            </div>
+          </div> */}
+          <div className='box' key="2" data-grid={{ w: 2, h: 5, x: 0, y: 0, minW: 2, minH: 5 }}>
+            <NumberDisplay number="13" statistic="Pull Requests Created"/>
           </div>
-          <NumberDisplay number="13" statistic="Pull Requests Created"/>
-          <NumberDisplay number="6" statistic="Pull Requests Reviewed"/>
-          <h3>List of Branches</h3>
-          {this.props.branches.map((branchName) => {
-            return <p>{branchName}</p>
-          })}
-          <div className="chart chart-bar">
-              <span className="chart-title">(Redux) Commits Per Member</span>
-              <BarChart chartData={this.props.commitChartData}/>
+          <div className='box' key="3" data-grid={{ w: 2, h: 5, x: 2, y: 0, minW: 2, minH: 5 }}>
+            <NumberDisplay number="6" statistic="Pull Requests Reviewed"/>
           </div>
+          <div className='box' key="4" data-grid={{ w: 2, h: 9, x: 0, y: 0, minW: 2, minH: 5 }}>
+            <h3>List of Branches</h3>
+            {this.props.branches.map((branchName) => {
+              return <p>{branchName}</p>
+            })}
+          </div>
+          <div className='box' key="5" data-grid={{ w: 3, h: 5, x: 4, y: 0, minW: 2, minH: 5 }}>
+            <div className="chart">
+                <span className="chart-title">(Redux) Commits Per Member</span>
+                <Bar data={this.props.commitChartData} options={{maintainAspectRatio: false, responsive: true}}/>
+            </div>
+          </div>
+        </GridLayout>
       </div>
     );
   }
