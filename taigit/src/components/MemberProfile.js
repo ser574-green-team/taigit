ee3e99c4bea8282e5a41b11966bdaeacf2a54eb5
Trevor Forrey <trevor4e@gmail.com>
2019-03-15 12:15:10 -0700

    [US-82 Task-170] Saving Member Profile to local stoage

--- taigit/src/components/MemberProfile.js
+++ taigit/src/components/MemberProfile.js
@@ -2,16 +2,46 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import TeamMemberCard from './TeamMemberCard'
 import NumberDisplay from './NumberDisplay'
-import GridLayout from 'react-grid-layout';
+import ReactGridLayout from 'react-grid-layout';
 import { Radar } from 'react-chartjs-2';
+import { saveToLocalStorage, getFromLocalStorage } from '../utils/utils';
+
+const layoutname = 'member-layout';
+const originalLayout = getFromLocalStorage(layoutname, 'layout') || [];
 
 class MemberProfile extends Component {
+  static defaultProps = {
+    className: "layout",
+    cols: 12,
+    rowHeight: 10,
+    width: 1200,
+    onLayoutChange: function() {}
+  };
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      layout: JSON.parse(JSON.stringify(originalLayout))
+    };
+
+    this.onLayoutChange = this.onLayoutChange.bind(this);
+  }
+
+  onLayoutChange(layout) {
+    console.log('about to save to local storage');
+    saveToLocalStorage(layoutname, "layout", layout);
+    this.setState({layout});
+    console.log('new layout', layout);
+    this.props.onLayoutChange(layout);
+  }
+
   render() {
     let member = this.props.teamMembers.filter(member => member.githubId == this.props.match.params.memberId)[0];
     return(
       <div className="app-page">
         <h2>TeamMemberId: {this.props.match.params.memberId}</h2>
-        <GridLayout className="layout" cols={12} rowHeight={30} width={1200}>
+        <ReactGridLayout layout={this.state.layout} onLayoutChange={this.onLayoutChange} cols={12} rowHeight={30} width={1200}>
           <div className='box' key="1" data-grid={{ w: 3, h: 9, x: 0, y: 0, minW: 2, minH: 3 }}>
             <div className="chart">
               <TeamMemberCard taigaId={member.taigaId} githubId={member.githubId} name={member.name} pictureUrl={member.pictureUrl}/>
@@ -31,7 +61,7 @@ class MemberProfile extends Component {
           <div className='box' key="5" data-grid={{ w: 2, h: 5, x: 9, y: 0, minW: 2, minH: 3 }}>
             <NumberDisplay number="23" statistic="Tasks Completed"/>
           </div>
-        </GridLayout>
+        </ReactGridLayout>
       </div>
     );
   }
