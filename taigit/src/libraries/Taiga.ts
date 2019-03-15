00bf6103217a8afb02fb7c44de92324d59411802
Cecilia La Place <laplace95@gmail.com>
2019-03-14 17:27:43 -0700

    Completed all function rewrites.
    Role ID identification remaining

--- taigit/src/libraries/Taiga.ts
+++ taigit/src/libraries/Taiga.ts
@@ -40,12 +40,22 @@ project_info(slug : string) : Promise<Object> {
  * @param projId the ID for project to get stats of
  * @returns project stats object
  * {
+ *      assigned_pts: number,               // Number of assigned points
+ *      assigned_pts_per_role: Object[],    // Distribution of assigned points across roles
+ *      closed_pts: number,                 // Number of closed points
+ *      closed_pts_per_role: Object[] //TODO: Figure out Role IDs,
+ *      num_sprints: number,                // Number of sprints
+ *      total_pts: number,                  // Total points in the project
  * }
  */
 export async function
 project_stats(projId : number) : Promise<Object> {
     let data = await axios.get("https://api.taiga.io/api/v1/projects/" + projId.toString() + '/stats');
-
+    let info : {assigned_pts: number, assigned_pts_per_role: Object[], closed_pts: number, closed_pts_per_role: Object[],
+                num_sprints: number, total_pts: number} = 
+            {assigned_pts: data.data.assigned_points, assigned_pts_per_role: data.data.assigned_points_per_role, 
+            closed_pts: data.data.closed_points, closed_pts_per_role: data.data.closed_points_per_role,
+            num_sprints: data.data.total_milestones, total_pts: data.data.total_points};
     return (data.data);
 }
 
@@ -54,30 +64,29 @@ project_stats(projId : number) : Promise<Object> {
  * @param sprintId the ID for the sprint to get stats for
  * @returns sprint stats object
  * {
-
+ *      completed_pts: Object[],    // Distribution of completed points across roles
+ *      total_pts: Object[],        // Total number of points
+ *      completed_tsks: number,     // Number of completed tasks
+ *      total_tsks: number,         // Total number of tasks
+ *      completed_us: number,       // Number of completed user stories
+ *      total_us: number,           // Total number of user stories
+ *      sprint_start: string,       // Sprint start date as a string
+ *      sprint_end: string,         // Sprint end date as a string
+ *      burndown: Object[],         // Array object containing day, open_points, optimal_points
  * }
  */
 export async function
 sprint_stats(sprintId : number) : Promise<Object> {
     let data = await axios.get("https://api.taiga.io/api/v1/milestones/"+sprintId.toString()+ '/stats');
-     return (data.data);
-}
-// TODO: This API calls are not useful to frontend and Taiga and will be deprecated.
-//This call returns user story  stats based on userstory Id
-export async function
-userstory_statuses(userstoryId : number) : Promise<Object> {
-    let data = await axios.get("https://api.taiga.io/api/v1/userstory-statuses/"+userstoryId.toString());
-    //test link:  https://api.taiga.io/api/v1/userstory-statuses/1124228
-    return (data.data)
-}
-
-// TODO: This API calls are not useful to frontend and Taiga and will be deprecated.
-//This call returns task stats based on task Id
-export async function
-task_statuses(taskId : number) : Promise<Object> {
-    let data = await axios.get("https://api.taiga.io/api/v1/task-statuses/" + taskId.toString());
-    //test link:  https://api.taiga.io/api/v1/task-statuses/1550500
-    return (data.data)
+    let info : {completed_pts: Object[], total_pts: number, completed_tsks: number, total_tsks: number, 
+                completed_us: number, total_us: number, sprint_start: string, sprint_end: string,
+                burndown: Object[]} = 
+            {completed_pts: data.data.completed_points, total_pts: data.data.total_points, 
+            completed_tsks: data.data.completed_tasks, total_tsks: data.data.total_tasks,
+            completed_us: data.data.completed_userstories, total_us: data.data.total_userstories,
+            sprint_start: data.data.estimated_start, sprint_end: data.data.estimated_finish,
+            burndown: data.data.days};
+    return (info);
 }
 
 /**
