653bf2fac6c386b7b3035556fd6b8bdf5cfc68f4
dlahtine <dlahtine@asu.edu>
2019-03-16 00:38:28 -0700

    tested functions; seem to work properly.

--- taigit/src/libraries/GitHub/src/findBuilds.ts
+++ taigit/src/libraries/GitHub/src/findBuilds.ts
@@ -12,7 +12,8 @@ usesGradle(owner: string, repo: string) : Promise<boolean>{
 
 export async function
 usesCMake(owner: string, repo: string) : Promise<boolean>{
-    return await fileInRepo(owner, repo, "CMakeFiles.txt");
+    console.log("made it here!");
+    return await fileInRepo(owner, repo, "CMakeLists.txt");
 }
 
 export async function
@@ -26,10 +27,12 @@ usesMake(owner: string, repo: string) : Promise<boolean>{
 }
 
 async function fileInRepo(owner: string, repo: string, filename: string): Promise <boolean>{
+    console.log("gonna find the file in the repo!");
     try{
-        const latestCommit = await axios.get("https://api.github.com/repos/" + owner + 
-        "/" + repo + "/git/commits/master");
-        let rootSha = latestCommit.data.tree.sha;
+        const branchInfo = await axios.get("https://api.github.com/repos/" + owner + 
+        "/" + repo + "/branches/master");
+        console.log("got latest commit!");
+        let rootSha = branchInfo.data.commit.sha;
         return await fileInTree(owner, repo, rootSha, filename);
     } catch (error){
         console.log(error);
@@ -40,17 +43,20 @@ async function fileInRepo(owner: string, repo: string, filename: string): Promis
 async function fileInTree(owner: string, repo: string, treeSha: string, filename: string): Promise<boolean>{
     const tree = await axios.get("https://api.github.com/repos/" + owner + "/" +
         repo + "/git/trees/" + treeSha);
-
-    for (let gitObject of tree.data){
+    console.log("Got tree!: " + tree.data.tree);
+    for (let gitObject of tree.data.tree){
         //if it's a blob, check for pom.xml
-        if (gitObject.type.localCompare("blob") == 0){
+        if (gitObject.type === "blob"){
             let objPath = gitObject.path.split("/");
             let objName = objPath[objPath.length -1];
-            if (objName.localCompare(filename) == 0){
+            console.log(objName);
+            if (objName === filename){
+                console.log("FOUND IT!!!!");
                 return true;
             }
-        } else if (gitObject.type.localCompare("tree") == 0){
-            if (fileInTree(owner, repo, gitObject.sha, filename)){
+        } else if (gitObject.type === "tree"){
+            if (await fileInTree(owner, repo, gitObject.sha, filename)){
+                console.log("DSJF:SOIUHEEN:FES:OINF");
                 return true;
             }
         }
