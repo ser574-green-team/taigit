d7b2ac960b53faa9a2eda69d87568fe0e2ca6061
Ruby Zhao <rubyqzhao@gmail.com>
2019-03-16 16:41:21 -0700

    Added some changes to the project page formatting (Task 86)

--- taigit/src/components/Projects.js
+++ taigit/src/components/Projects.js
@@ -9,30 +9,32 @@ export default class Projects extends Component {
                 <h2>Projects</h2>
                 <ProjectPanel projUrl="/"
                               projName="Team Broccoli"
-                              memberUrl="/team/members/broutzong"
-                              member="Bailey Routzong" //, "Amy Koffee", "Miguel Smith", "Mr Wicked"
+                              memberUrl={["/team/members/broutzong",""]}
+                              member={["Bailey Routzong", "Amy Koffee"]}
                 />
+                <br/>
+                <h2>New Project</h2>
                 <div className="form">
                     <form style={{display: 'absolute'}}>
                         <input type="text"
                                name="repo-name"
-                               style={{flex: '10', padding: '5px', width: '200px', right: '5px'}}
+                               style={{flex: '10', padding: '12px 20px', width: '20%', margin: '0 8px'}}
                                placeholder="GitHub repository name"
                         />
                         <input type="text"
                                name="repo-owner"
-                               style={{flex: '10', padding: '5px', width: '200px', right: '5px'}}
+                               style={{flex: '10', padding: '12px 20px', width: '20%', margin: '0 8px'}}
                                placeholder="GitHub repository owner"
                         />
                         <input type="text"
                                name="taiga"
-                               style={{flex: '10', padding: '5px', width: '200px', right: '5px'}}
+                               style={{flex: '10', padding: '12px 20px', width: '20%', margin: '0 8px'}}
                                placeholder="Taiga project ID"
                         />
                         <input type="submit"
                                value="Add Project"
                                className="btn"
-                               style={{flex: '1'}}
+                               style={{flex: '1', margin: '8px 4px', border: 'none'}}
                         />
                     </form>
                 </div>
