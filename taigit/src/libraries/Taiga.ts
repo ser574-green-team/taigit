9f0e540f4bf95a78cc2980f6008d96efcd86f6e2
xiangweizheng <43075724+xiangweizheng@users.noreply.github.com>
2019-03-15 20:59:56 -0700

    resolve conflict with dev 6th try for US139 Task 152
    resolve conflict with dev 6th try for US139 Task 152

--- taigit/src/libraries/Taiga.ts
+++ taigit/src/libraries/Taiga.ts
@@ -27,3 +27,357 @@ project_stats(projId : number) : Promise<Object> {
     
     return (data.data);
 }
+
+
+
+/**
+
+
+
+
+* @summary This call assess a task based on task_assessment_state_trans based on task Id
+
+
+* @param taskId the ID for the task to assess task
+
+
+* @returns  whether the task's state transition is valid,task status transition, date based on task Id
+
+
+* * {
+
+
+*      state_trans_invalid : Boolean       // true, the task state transition is not valid; false, the task is valid.
+
+
+*      date : number,         // Date and time of history entry in milliseconds since epoch
+
+
+*      user : object,         // Taiga User Object
+
+
+*      status_trans:entry.String[],//Status transition array
+
+
+*
+
+
+* }
+
+
+*/
+
+
+export async function
+
+
+task_assessment_state_trans(taskId : number) : Promise<Object> {
+
+
+let data = (await axios.get(`https://api.taiga.io/api/v1/history/task/${taskId}`)).data;
+
+
+   //test case :https://api.taiga.io/api/v1/history/task/2577741
+
+
+let output : Array<Object> = [];
+
+
+for(let entry of data) {
+
+
+   let new_entry = {
+
+
+       state_trans_invalid : false,
+
+
+       date : new Date(entry.created_at).getTime(),
+
+
+       user : entry.user,
+
+
+       status_trans:entry.values_diff.status
+
+
+   }
+
+
+
+
+
+
+
+   //Only three status transistion is valid
+
+
+   //which is  ["New", "In progress"] ["In progress", "Ready for test"]  ["Ready for test", "Closed"])
+
+
+   let old_status = new_entry.status_trans[0];
+
+
+   let new_status = new_entry.status_trans[1];
+
+
+   if((old_status == "New" && new_status != "In progress") ||
+
+
+       (old_status == "In progress"    && new_status != "Ready for test") ||
+
+
+       (old_status == "Ready for test" && new_status != "Closed"))
+
+
+       new_entry.state_trans_invalid = true;
+
+
+   output.push(new_entry);
+
+
+}
+
+
+
+
+
+
+
+return output;
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+/**
+
+
+* @summary This call return a task current end status and estimated finished percentage based on task Id
+
+
+* @param taskId the ID for the task to assess task
+
+
+* @returns  whether the task is abnormal,task status transition, date based on task Id
+
+
+* * {
+
+
+*      finished : Boolean           // true, the task is finished.
+
+
+*      fin_per : number            // finished percentage % default [0%,10%,60%,100%]
+
+
+*      end_status : string,       // current task status  ["New", "In progress","Ready for test", "Closed"]
+
+
+*      num_stat : number           // current task status [0 1 2 3] corresponding to string in end_status
+
+
+*
+
+
+* }
+
+
+*/
+
+
+export async function
+
+
+task_assessment_endstate(taskId : number) : Promise<Object> {
+
+
+
+
+
+
+
+   let data = (await axios.get(`https://api.taiga.io/api/v1/history/task/${taskId}`)).data;
+
+
+   //test case :https://api.taiga.io/api/v1/history/task/2577741   or 2555550
+
+
+   let output : Array<Object> = [];
+
+
+   let endstatus : string = "New";
+
+
+   for(let entry of data) {
+
+
+       let new_entry = {
+
+
+           state_trans_invalid : false,
+
+
+           date : new Date(entry.created_at).getTime(),
+
+
+           user : entry.user,
+
+
+           status_trans : entry.values_diff.status
+
+
+       }
+
+
+       endstatus = new_entry.status_trans[1];
+
+
+       output.push(new_entry);
+
+
+   }
+
+
+
+
+
+
+
+   let t_finished : boolean = (endstatus == "Closed")?
+
+
+   let t_fin_per : number;
+
+
+   let t_num_stat : number;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   //transfer ["New", "In progress"] ["In progress", "Ready for test"]  ["Ready for test", "Closed"])
+
+
+   //into number status
+
+
+   switch(endstatus){
+
+
+       case "Closed" : {
+
+
+           t_fin_per = 100;
+
+
+           t_num_stat = 3;
+
+
+           break
+
+
+       }
+
+
+       case "Ready for test" : {
+
+
+           t_fin_per = 60;
+
+
+           t_num_stat = 2;
+
+
+           break
+
+
+       }
+
+
+       case "In progress": {
+
+
+           t_fin_per = 10;
+
+
+           t_num_stat = 1;
+
+
+           break
+
+
+       }
+
+
+       case "New" : {
+
+
+           t_fin_per =  0;
+
+
+           t_num_stat = 0;
+
+
+           break
+
+
+       }
+
+
+       default : {
+
+
+           t_fin_per = 0;
+
+
+           t_num_stat = 0
+
+
+           break;
+
+
+       }
+
+
+   }
+
+
+
+
+
+
+
+   let info : {finished : boolean, end_status : string,num_stat : number,fin_per : number}
+
+
+   = {finished : t_finished, end_status : endstatus, num_stat : t_num_stat, fin_per : t_fin_per};
+
+
+   return (info);
+
+
+}
+
