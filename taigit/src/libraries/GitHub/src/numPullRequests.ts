7ec97d15856f9c9330f964be46767faea9537ede
Sanay Devi <sanaydevi@Sanays-MacBook-Air.local>
2019-04-01 21:28:18 -0700

    @Github team : Earlier code was return 9 as the value, however after incorporating pagination in lines 12-16, was able to get actual number of pull requests, Please fix this bug in other codes wherever necessary

--- taigit/src/libraries/GitHub/src/numPullRequests.ts
+++ taigit/src/libraries/GitHub/src/numPullRequests.ts
@@ -3,12 +3,23 @@ import axios from 'axios';
 export async function
 getNumPullRequests(owner: string, repo: string, auth: string){
     try{
+        let numOfPR = 0
         var config = {
             headers: {'Authorization': "Bearer " + auth}
         }
         const pulls = await axios.get("https://api.github.com/repos/" + owner +
-            "/" + repo + "/pulls", config);
-        return pulls.data.length;
+            "/" + repo + "/pulls?state=all&direction=asc", config);
+        let last = pulls.headers["link"].split(',');
+        let total_pages_str = last[1]
+        let total_pages = total_pages_str.substring(total_pages_str.lastIndexOf("page") + 5,total_pages_str.lastIndexOf(">"))
+        total_pages = Number(total_pages)
+        for(var j =1 ; j <= total_pages;j++){
+            const innerpulls = await axios.get("https://api.github.com/repos/" + owner +
+                "/" + repo + "/pulls?state=all&direction=asc&page=j", config);
+            numOfPR += innerpulls.data.length
+
+        }
+        return numOfPR
     } catch (error) {
         console.log(error);
     }
