2ca4bfa38eb3f42864cfb58340b160d6373ff5d8
Aditya Samant <adityasamant2505@gmail.com>
2019-04-10 23:26:22 -0700

    US#111Task#281 & 282 refactored so that all getCommits methods are present in one file, added new commit method to get total number of commits, refactored to get rid of cyclomatic complexity and refactored codeAnalysis to get rid of codacy token

--- taigit/src/libraries/GitHub/GitHub.js
+++ taigit/src/libraries/GitHub/GitHub.js
@@ -1,8 +1,7 @@
 import {gethistoryPR} from "./src/getHistoryOfPR";
 
-export {getNumCommitsFromUser} from "./src/numCommitsFromUser";
 export {getBranches, getNumBranchCommits} from "./src/branches";
-export {getCommitsInTimeWindow} from "./src/commitsInWindow";
+export {getCommitsInTimeWindow, getTotalCommits, getNumCommitsFromUser} from "./src/commitsInWindow";
 export {getNumPullRequests} from "./src/numPullRequests";
 export {getNumComments} from "./src/avgComments";
 export {contributorData} from "./src/contributorData";
@@ -12,7 +11,6 @@ export {getCodeAnalysis} from "./src/codeAnalysis";
 export {usesMaven, usesGradle, usesCMake, usesMake, usesAnt, getBuilds} from "./src/findBuilds";
 export {getMemberInfo} from "./src/getMemberInfo"
 export {gethistoryPR} from "./src/getHistoryOfPR"
-export {getMcCabeComplexity} from "./src/cyclomaticComplexity";
 export {getUserRepos} from './src/reposAvailable';
 export {getUserInfo} from './src/getUserInfo';
 
